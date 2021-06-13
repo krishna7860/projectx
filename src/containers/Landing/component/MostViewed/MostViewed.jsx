dc483a5f73f729a3f98f929a8e23a8cd9d0fd391
Krishna Sharma <krishna.sharma@shipsy.co.in>
2021-06-13 16:19:34 +0530

    Added the rule

--- src/containers/Landing/component/MostViewed/MostViewed.jsx
+++ src/containers/Landing/component/MostViewed/MostViewed.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-use-before-define */
 /* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-props-no-spreading */
