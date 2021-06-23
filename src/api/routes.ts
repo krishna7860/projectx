3dd2e86910d816e8597ce0b878891b751babf904
Krishna Sharma <krishna.sharma@shipsy.co.in>
2021-06-23 15:47:35 +0530

    fixed issues in failing

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -1,4 +1,5 @@
-export const BACKEND_BASE_URL = "http://localhost:5001";
+export const BACKEND_BASE_URL =
+  "http://projectxbackend-env.eba-m2h5ycuq.us-east-1.elasticbeanstalk.com";
 export const STATES_ROUTES = "api/v1/states";
 export const CITIES_ROUTES = "api/v1/cities";
 export const AUTH_ROUTES = "api/v1/auth";
